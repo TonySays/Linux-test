515e013494cdf5415a9782f1d5c54f8949cae0dc
Tonysays <29088173+TonySays@users.noreply.github.com>
2022-03-09 21:39:58 +1100

    tutorial 14a

--- python_tutorial.py
+++ python_tutorial.py
@@ -1,3 +1,4 @@
+from ast import Return
 from audioop import reverse
 from cgi import print_environ_usage
 from itertools import count
@@ -5,22 +6,20 @@ import itertools
 
 from sqlite3 import Row
 
-
-
-game = [[2, 1, 1],
-        [2, 1, 1],
-        [1, 1, 2],]
-
-
-
-
 def win(current_game):
     # Check for horizontal wins
 
+    def all_same(l):
+        if l.count(l[0]) == len(l) and l[0] !=0:
+            return True
+        else:
+            return False
+
     for row in game:
         print(row)
-        if row.count(row[0]) == len(row) and row[0] !=0:  # Check the no, of times row 0 value appears
+        if all_same(row):
             print(f"player {row[0]} wins horizontally")
+            return True  # Return True then function stops here, otherwise it will keep runing the rest of Code 
 
 
     # Record reverse diagnoal
@@ -32,18 +31,18 @@ def win(current_game):
     for col, row in zip(cols,rows):
         digas.append(game[row][col])
 
-    if digas.count(digas[0]) == len(digas) and digas[0] !=0:  # Check the no, of times row 0 value appears
+    if all_same(digas):
         print(f"player {digas[0]} wins diagonally (/)!")
+        return True
    
-
     # Checks for diagnoal 
-
     digas = []
     for ix in range(len(game)):
         digas.append(game[ix][ix])
 
-    if digas.count(digas[0]) == len(digas) and digas[0] !=0: # Check the no, of times row 0 value appears
+    if all_same(digas): # Check the no, of times row 0 value appears
         print(f"player {digas[0]} wins diagonally (\\)!")
+        return True
 
     # Check for vertical wins
     for col in range(len(game)):
@@ -54,8 +53,11 @@ def win(current_game):
 
             check.append(row[col])
         
-        if check.count(check[0]) == len(check) and check[0] != 0 and len(check) >= len(game):
+        if all_same(check):
             print(f"player {check[0]} wins vertically (|)!")
+            return True
+
+    return False
 
 
 def game_board(
@@ -64,6 +66,9 @@ def game_board(
   # Hanging indentation
 
     try:
+        if game_ini[row][column]!=0:
+            print("This position is occupied, choose another!")
+            return game_ini, False
         print("   0  1  2")
     
         if not just_display:
@@ -71,13 +76,15 @@ def game_board(
             
         for unpack1,unpack2 in enumerate(game_ini):
             print(unpack1,unpack2)
-        return game_ini
+        return game_ini, True
 
     except IndexError as e:
         print("error input row/column out of range try 0,1,2 ->",e)
+        return game_ini, False, 
 
     except Exception as e:
         print("something something wrong ->",e)
+        return game_ini, False
 
 play = True
 
@@ -89,15 +96,44 @@ while play:
             [0, 0, 0],]
 
     game_won = False
-    game = game_board(game,just_display=True)        # Pass in game array as variable 
+    game, _ = game_board(game,just_display=True)        # Pass in game array as variable 
     player_choice = itertools.cycle([1,2])
 
     while not game_won:
         current_player = next(player_choice)
         print(f"current player is {[current_player]}")
-        column_choice = int(input("what column you want to play? (0,1,2):"))
-        row_choice = int(input("what row you want to play? (0,1,2):"))
-        game = game_board(game, current_player, row_choice, column_choice)
+        played = False
 
+        while not played:
+            column_choice = int(input("what column you want to play? (0,1,2):"))
+            row_choice = int(input("what row you want to play? (0,1,2):"))
+            game, played = game_board(game, current_player, row_choice, column_choice)
+        
+        if win(game):
+            game_won = True
+            again = input("Game over, want to play again? (y/n) ")
+#  My methods
+            if again.lower() != "y":
+                print("game end")
+                play = False
+            else:
+                print("restarting the game now!")
+
+''' Tutorial methods
+            if again.lower() == "y":
+                print("restarting now")
+            elif again.lower() == "n":
+                print("game end")
+                play = False
+            else:
+                print("I dont understand")
+                play = False
+'''         
+            
+            
+
+
+
+            
 
 # game_board(game_board,player=1,row=2,column=2)  # Row out of range
