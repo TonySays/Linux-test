ac95796a4fa059a56a683bb923cce76da0282eed
Tonysays <29088173+TonySays@users.noreply.github.com>
2022-04-03 19:21:26 +1000

    udpate Django with tinymce

--- Django/mysite/main/models.py
+++ Django/mysite/main/models.py
@@ -1,10 +1,12 @@
+from socket import TIPC_IMPORTANCE
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
-    tutorial_published = models.DateTimeField("date published")
+    tutorial_published = models.DateTimeField("date published", default= datetime.now)
 
 def _str_(self):
     return self.tutorial_title 
